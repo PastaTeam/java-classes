c29db6d3ff11f92faaa023d05d475608c2cd8beb
Alekos Filini <alekos.filini@gmail.com>
2017-03-27 11:49:06 +0200

    Aggiunta DatabaseConnection

---
+++ src/com/pastateam/utils/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package com.pastateam.utils;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static Connection connection = null;
+
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://<host>:<porta>/<dbname>"; // FIXME: cambiare
+
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    private static void createConnection() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        if (connection == null)
+            createConnection();
+
+        return connection;
+    }
+}
