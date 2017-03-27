1b919960f7a0d6e1f5d2685a16775ca73dbe15b1
drTerrible <gabriele.rox97@gmail.com>
2017-03-27 12:34:35 +0200

    ignora classe connessione

--- src/com/pastateam/utils/DatabaseConnection.java
+++ src/com/pastateam/utils/DatabaseConnection.java
@@ -6,7 +6,7 @@ public class DatabaseConnection {
     private static Connection connection = null;
 
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://<host>:<porta>/<dbname>"; // FIXME: cambiare
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/smau"; // FIXME: cambiare
 
     private static final String USER = "root";
     private static final String PASS = "";
