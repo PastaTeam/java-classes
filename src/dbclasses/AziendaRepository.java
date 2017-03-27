09f0c3410c346299dd13960abd8bcf7cab6a661b
Luca Mimini <assassinluk@gmail.com>
2017-03-27 12:39:14 +0200

    Modifica

--- src/dbclasses/AziendaRepository.java
+++ src/dbclasses/AziendaRepository.java
@@ -5,10 +5,29 @@
  */
 package dbclasses;
 
+import com.pastateam.dbinterface.IAziende;
+import com.pastateam.model.Azienda;
+import java.util.List;
+
 /**
  *
  * @author FVD
  */
-public class AziendaRepository {
+public class AziendaRepository implements IAziende{
+
+    @Override
+    public List<Azienda> getListaAziende() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Azienda getAziendaFromID(Integer ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Azienda getAziendaFromEmail(String email) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
